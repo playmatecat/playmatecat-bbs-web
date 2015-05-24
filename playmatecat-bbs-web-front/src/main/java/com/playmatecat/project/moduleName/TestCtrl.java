d04095c402c643ec23215a1cd02b45f3b5fd0b08
Isa <Isa@Isa-PC>
2015-05-24 20:01:56 +0800

    cas done

--- playmatecat-bbs-web-front/src/main/java/com/playmatecat/project/moduleName/TestCtrl.java
+++ playmatecat-bbs-web-front/src/main/java/com/playmatecat/project/moduleName/TestCtrl.java
@@ -1,46 +1,22 @@
-//package com.playmatecat.project.moduleName;
-//
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.servlet.ModelAndView;
-//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-//
-//import com.playmatecat.modules.login.vo.LoginVO;
-//import com.playmatecat.utils.encrypt.UtilsAES;
-//
-//@RestController
-//@RequestMapping("")
-//public class TestCtrl {
-//	
-//	@RequestMapping("/testPage")
-//	public ModelAndView testPage(Model model) {
-//		ModelAndView mav = new ModelAndView("testModule.testMain");
-//		model.addAttribute("testParam", "testModelParams");
-//		return mav;
-//	}
-//	
-//	@RequestMapping("/cas-login")
-//	public ModelAndView casLogin(LoginVO loginVO, Model model, RedirectAttributes redirectAttributes) {
-//		System.out.println("do-cas-login");
-//		
-//		//解密后形式 用户名,密码,单点登录时间
-//		//String encryptTicket = UtilsAES.decrypt(loginVO.getTicket());
-//		
-//		String encryptTicket = UtilsAES.decrypt(loginVO.getUrl());
-//		
-//		String ticketArr[] = encryptTicket.split(",");
-//		String username = ticketArr[0];
-//		String password = ticketArr[1];
-//		
-////		Subject subject = SecurityUtils.getSubject();
-////		AuthenticationToken token = new UsernamePasswordToken(username,password);
-//		
-//		
-//		ModelAndView mav = new ModelAndView( "redirect:" + loginVO.getUrl());
-//		model.addAttribute("testParam", "testModelParams");
-//		redirectAttributes.addAttribute("testParam", "testModelParams");
-//		
-//		return mav;
-//	}
-//}
+package com.playmatecat.project.moduleName;
+
+import org.apache.shiro.SecurityUtils;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("")
+public class TestCtrl {
+	
+	@RequestMapping("/testPage")
+	public ModelAndView testPage(Model model) {
+		SecurityUtils.getSubject().isPermitted("test:test");
+		ModelAndView mav = new ModelAndView("testModule.testMain");
+		model.addAttribute("testParam", "testModelParams");
+		return mav;
+	}
+	
+
+}
