890e3ff70dd3b9e4b3fee395fcf27faea979809f
blackcatIan <blackcatIan@hotmail.com>
2015-05-26 16:10:46 +0800

    1

--- playmatecat-bbs-web-front/src/main/java/com/playmatecat/project/moduleName/TestCtrl.java
+++ playmatecat-bbs-web-front/src/main/java/com/playmatecat/project/moduleName/TestCtrl.java
@@ -1,6 +1,7 @@
 package com.playmatecat.project.moduleName;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("")
 public class TestCtrl {
 	
+    @RequiresPermissions(value={"test:test:view"})
 	@RequestMapping("/testPage")
 	public ModelAndView testPage(Model model) {
 		SecurityUtils.getSubject().isPermitted("test:test");
@@ -18,5 +20,12 @@ public class TestCtrl {
 		return mav;
 	}
 	
-
+    @RequiresPermissions(value={"test:test:view1"})
+    @RequestMapping("/testPage1")
+    public ModelAndView testPage1(Model model) {
+        SecurityUtils.getSubject().isPermitted("test:test");
+        ModelAndView mav = new ModelAndView("testModule.testMain");
+        model.addAttribute("testParam", "testModelParams");
+        return mav;
+    }
 }
