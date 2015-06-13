d5724412883c607ca8c5c05ee4cdba8d66b8c98f
Isa <Isa@Isa-PC>
2015-06-13 23:19:29 +0800

    1

--- playmatecat-bbs-web-admin/src/main/java/com/playmatecat/ctrl/authorization/RoleAdminController.java
+++ playmatecat-bbs-web-admin/src/main/java/com/playmatecat/ctrl/authorization/RoleAdminController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.http.HttpResponse;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,10 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.playmatecat.bbs.bbsApp.admin.vo.AuthorizationVO;
+import com.playmatecat.database.bbs.Article;
 import com.playmatecat.domains.sysBBS.dto.RoleDTO;
+import com.playmatecat.mina.NioTransferAdapter;
 import com.playmatecat.utils.label.UtilsGUID;
+import com.playmatecat.utils.mina.UtilsNioClient;
 
-@RequestMapping("/admin/role-admin")
+/**
+ * 角色管理
+ * @author blackcat
+ *
+ */
+@RequestMapping("/admin/authorization/role-admin")
 @RestController
 public class RoleAdminController {
 
@@ -25,6 +32,7 @@ public class RoleAdminController {
     public ModelAndView getRoles(AuthorizationVO authorizationVO, Model model,
             HttpServletRequest request, HttpServletResponse response) {
         
+        UtilsNioClient.write("bbs", new NioTransferAdapter("1", "{'content':'abc!!!!'}", Article.class) );
         //call mina service
         List<RoleDTO> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
